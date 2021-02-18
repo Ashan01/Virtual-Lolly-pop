4ef2a9566b8e7579538f7bdbaa80ffb59461595f
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-02-18 18:36:17 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,28 @@ import React, { useState } from "react";
 import Lolly from "../components/lolly";
 
 const Home = () => {
-   let [c1, setC1] = useState();
+   let [c1, setC1] = useState("#FF0000");
+   let [c2, setC2] = useState("#800080	");
+   let [c3, setC3] = useState("#FF00FF	");
+
    return (
       <div>
-         <Lolly />
+         <Lolly top={c1} middle={c2} bottom={c3} />
 
          <div>
-            <input type="color" />
-            <input type="color" />
-            <input type="color" />
+            <input type="color" onChange={(e) => setC1(e.target.value)} />
+            <input type="color" onChange={(e) => setC2(e.target.value)} />
+            <input type="color" onChange={(e) => setC3(e.target.value)} />
+         </div>
+
+         <div className="form-container">
+            <input type="text" placeholder="To" ref={recField} />
+            <textarea
+               placeholder="Enter your message!"
+               ref={msgField}
+            ></textarea>
+            <input type="text" placeholder="From" ref={senderField} />
+            <button onClick={handleSubmit}>Send</button>
          </div>
       </div>
    );
